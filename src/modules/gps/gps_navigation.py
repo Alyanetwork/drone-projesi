f5917771fdd7201aa880a1768e1d3dc4a569d717
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 02:24:32 +0300

    Update gps_navigation.py
    GPS ile otonom uçuş sınıfı.

--- src/modules/gps/gps_navigation.py
+++ src/modules/gps/gps_navigation.py
@@ -1,1 +1,16 @@
+# gps_navigation.py
 
+from dronekit import LocationGlobalRelative
+from config import WAYPOINTS
+
+class GPSNavigation:
+    def __init__(self, vehicle):
+        self.vehicle = vehicle
+
+    def goto_location(self, latitude, longitude, altitude):
+        target_location = LocationGlobalRelative(latitude, longitude, altitude)
+        self.vehicle.simple_goto(target_location)
+
+    def execute_mission(self):
+        for waypoint in WAYPOINTS:
+            self.goto_location(waypoint["latitude"], waypoint["longitude"], waypoint["altitude"])
