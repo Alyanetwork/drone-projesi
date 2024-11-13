5f7926ed74332c789c42323b452721b66607002d
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 01:41:43 +0300

    Create security.py

---
+++ security.py
@@ -0,0 +1,18 @@
+# security.py
+
+from Crypto.Cipher import AES
+import base64
+from config import AES_KEY
+
+def encrypt_message(message):
+    cipher = AES.new(AES_KEY, AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(message.encode('utf-8'))
+    return base64.b64encode(nonce + ciphertext).decode('utf-8')
+
+def decrypt_message(ciphertext):
+    data = base64.b64decode(ciphertext)
+    nonce = data[:16]
+    ciphertext = data[16:]
+    cipher = AES.new(AES_KEY, AES.MODE_EAX, nonce=nonce)
+    return cipher.decrypt(ciphertext).decode('utf-8')
