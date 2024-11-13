083611c8d25e5961c4aff0c3c7681a96cc7e4e1d
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 02:22:04 +0300

    Update route_planning.py
    Dinamik rota planlama sınıfı. Engel algılamaya göre rotayı günceller.

--- src/modules/ai_control/route_planning.py
+++ src/modules/ai_control/route_planning.py
@@ -1,1 +1,20 @@
+# route_planning.py
 
+from config import WAYPOINTS
+
+class RoutePlanning:
+    def __init__(self):
+        self.route = WAYPOINTS
+
+    def get_dynamic_route(self):
+        print("Dinamik Rota Oluşturuluyor...")
+        return self.route
+
+    def update_route(self, avoid_obstacle=False):
+        if avoid_obstacle:
+            print("Engellerden Kaçınmak İçin Yeni Rota Belirleniyor...")
+            self.route = self.generate_alternative_route()
+    
+    def generate_alternative_route(self):
+        print("Alternatif Rota Hesaplanıyor...")
+        return [{"latitude": 40.714276, "longitude": -74.006000, "altitude": 20}]
