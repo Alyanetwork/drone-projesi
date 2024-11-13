a0ef58bf8ad63d42b82fdeda4cbd83c0da0c0263
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 02:27:01 +0300

    Update jammer_control.py
    Sinyal bozucu kontrol sınıfı.

--- src/modules/security/jammer_control.py
+++ src/modules/security/jammer_control.py
@@ -2,7 +2,17 @@
 
 import RPi.GPIO as GPIO
 import time
-from config import
+from config import JAMMER_SETTINGS
+
+class JammerControl:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(JAMMER_SETTINGS["pin"], GPIO.OUT)
+
+    def activate(self):
+        GPIO.output(JAMMER_SETTINGS["pin"], GPIO.HIGH)
+        time.sleep(JAMMER_SETTINGS["duration"])
+        GPIO.output(JAMMER_SETTINGS["pin"], GPIO.LOW)
 
 
 
