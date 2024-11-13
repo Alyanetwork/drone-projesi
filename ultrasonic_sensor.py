6731aa1df035ac7bfb79eb7e11258fecb60e9d2c
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 01:53:59 +0300

    Create ultrasonic_sensor.py
    Ultrasonik sensörlerle engel algılama.

---
+++ ultrasonic_sensor.py
@@ -0,0 +1,23 @@
+# modules/sensors/ultrasonic_sensor.py
+
+import RPi.GPIO as GPIO
+import time
+from config import SENSOR_SETTINGS
+
+class UltrasonicSensor:
+    def __init__(self):
+        GPIO.setup(SENSOR_SETTINGS["ultrasonic"]["trig_pin"], GPIO.OUT)
+        GPIO.setup(SENSOR_SETTINGS["ultrasonic"]["echo_pin"], GPIO.IN)
+
+    def measure_distance(self):
+        GPIO.output(SENSOR_SETTINGS["ultrasonic"]["trig_pin"], True)
+        time.sleep(0.00001)
+        GPIO.output(SENSOR_SETTINGS["ultrasonic"]["trig_pin"], False)
+        
+        while GPIO.input(SENSOR_SETTINGS["ultrasonic"]["echo_pin"]) == 0:
+            start_time = time.time()
+        while GPIO.input(SENSOR_SETTINGS["ultrasonic"]["echo_pin"]) == 1:
+            stop_time = time.time()
+        
+        distance = (stop_time - start_time) * 34300 / 2
+        return distance < SENSOR_SETTINGS["ultrasonic"]["distance_threshold"]
